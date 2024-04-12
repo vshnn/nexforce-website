444aabe8039f1051fd507893be9cf890cd0dd8df
Vishnu V P <vishnuvayalilepurayil@gmail.com>
2024-04-12 13:49:52 +0530

    Finished stage 1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const header = document.querySelector('header');
+const header = document.querySelector('.header');
 const letters = document.querySelectorAll('.letter');
 const logo = document.querySelector('.logo a');
 const websiteContent = document.querySelector('.website-content');
+
 
 let lastscroll = 0;
 const sectionHeight = 150;
@@ -24,10 +25,60 @@ window.addEventListener('scroll', () => {
     ]
 
     orderPairs.forEach((pair, orderIndex) => {
-        const startScroll = sectiionHeight * orderIndex;
+        const startScroll = sectionHeight * orderIndex;
 
         if(scrollY >= startScroll){
-            const moveFacx
+            const moveFactor = Math.min(1, (scroll - startScroll) / sectionHeight);
+            const translateY = -moveFactor * header.offsetHeight;
+
+            pair.forEach((idx)=>{
+                const letter = letters[idx];
+                console.log(letter);
+                gsap.to(letter, {
+                    y: translateY,
+                    zIndex: 1-moveFactor,
+                });
+            });
+        }else{
+            pair.forEach((idx)=>{
+                const letter = letters[idx];
+                gsap.to(letter, {
+                    y: 0,
+                    zIndex: 1,
+                });
+            });
         }
-    });    
+    });
+    
+    const buffer = 50;
+    if(scrollY >= orderPairs.length * sectionHeight + buffer && 
+        !gsap.isTweening(logo)){
+            gsap.to(logo, {
+                top:"0px",
+                ease:"power1.out",
+                overwrite:"true"
+                
+            });
+
+            gsap.to(".logo-revealer",{
+                scaleY: 0,
+                overwrite: true,
+            });
+        } else if(
+            scrollY <= (orderPairs.length -1) * sectionHeight &&
+            !gsap.isTweening(logo)
+        )   {
+            gsap.to(logo, {
+                top: "20px",
+                ease: "power1.out",
+                overwrite: true,
+            });
+
+            gsap.to(".logo-revealer", {
+                scaleY: 1,
+                overwrite: true,
+            });
+        } 
+        lastscroll = scrollY;
+
 });
